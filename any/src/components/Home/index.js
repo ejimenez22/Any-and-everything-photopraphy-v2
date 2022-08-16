ee4152a4cd48cadfe5c3599f58fd06a4064c68af
Samantha Wolff <samanthawolff07@gmail.com>
2022-08-15 22:38:10 -0500

    install better-react-carousel and set up

--- any/src/components/Home/index.js
+++ any/src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from '../Nav'
-
+import Carousel from 'better-react-carousel'
 
 function Home() {
     return (
@@ -8,6 +8,17 @@ function Home() {
         <Nav />
         <div>
             <h1>Any & Everything Photography</h1>
+            <Carousel cols={2} rows={1} gap={10} loop>
+                <Carousel.Item>
+                    <img width="100%" src="../" alt="flowers"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width="100%" src="any/src/assets/images/DSC_1535.jpg" alt="car painting"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img width="100%" src="any/src/assets/images/DSC_1547.jpg" alt="car painting"/>
+                </Carousel.Item>
+            </Carousel>
         </div>
         </>
     )
