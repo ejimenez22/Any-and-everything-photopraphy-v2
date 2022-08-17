1d5a50da2ff834efe72e92e7bd09c89efe70de74
Enrique Jimenez <ejimenez2292@gmail.com>
2022-08-16 21:21:55 -0500

    adding email contact, rendering, needs functions

--- any/src/components/Contact/index.js
+++ any/src/components/Contact/index.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../Nav";
 
 function Contact() {
+    const [ name, setName ] = useState('')
+    const [ email, setEmail] = useState('')
+    const [ message, setMessage ] = useState('')
+    const [ emailSent, setEmailSent ] = useState('')
+
+    const submit = () => {
+        if (name && email && message) {
+            setName('')
+            setEmail('')
+            setMessage('')
+            setEmailSent('')
+        } else {
+            alert('Please fill in all fields.')
+        }
+    }
+
+    const isValidEmail = email => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    };
+
     return (
         <>
         <Nav />
-        <div>
-            <section>
-                <h1>Contact Me</h1>
-            </section>
-        </div>
+        <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+        <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
+        <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+        <button onClick={submit}>Send Message</button>
+        <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
         </>
     )
 }
