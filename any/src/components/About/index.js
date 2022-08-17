af578d42e368b862b2f3c1d460de48d88ea5c4f7
Enrique Jimenez <ejimenez2292@gmail.com>
2022-08-16 19:58:09 -0500

    adding about me...not sure what he wants to put yet

--- any/src/components/About/index.js
+++ any/src/components/About/index.js
@@ -7,6 +7,16 @@ function About() {
         <Nav />
         <section className="aboutMe" id="about">
             <h1>About Me</h1>
+            <div>
+                <img alt="family" src={require('../../assets/images/Joe-familia.jpg')}></img>
+            </div>
+            <div>
+                <article>
+                    <p>
+                        Something about Joe and why he takes pics...He's an Ok guy....he takes semi-decent pics ❤️
+                    </p>
+                </article>
+            </div>
         </section>
         </>
     )
