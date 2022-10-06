cc18a854835b81e6d526e81cbf7cc75d12f09ffa
Samantha Wolff <samanthawolff07@gmail.com>
2022-10-06 14:34:51 -0500

    style nav links

--- any/src/components/Nav/index.js
+++ any/src/components/Nav/index.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { slide as Menu } from 'react-burger-menu';
+// import { slide as Menu } from 'react-burger-menu'; //
 
 function Nav() {
 
     return (
-        <Menu>
-            <NavLink exact='true' to='/' className='menu-item'>
-                <h3>Home</h3>
-            </NavLink>
-            <NavLink exact='true' to='/about' className='mnu-item'>
-                <h3>About</h3>
-            </NavLink>
-            <NavLink exact='true' to='/contact' className='menu-item'>
-                <h3>Contact</h3>
-            </NavLink>
-            <NavLink exact='true' to='/portfolio' className='menu-item'>
-                <h3>Portfolio</h3>
-             </NavLink>
-        </Menu>
+
+        <div className="nav-container">
+            <div className="nav-links">
+                <div className='menu-item'>
+                    <NavLink exact='true' to='/'>
+                        <h3>Home</h3>
+                    </NavLink>
+                </div>
+                <div className='menu-item'>
+                    <NavLink exact='true' to='/about'>
+                        <h3>About Me</h3>
+                    </NavLink>
+                </div>
+                <div className='menu-item'>
+                    <NavLink exact='true' to='/contact'>
+                        <h3>Contact Me</h3> 
+                    </NavLink>
+                </div>
+                <div className='menu-item'>
+                    <NavLink exact='true' to='/portfolio'>
+                        <h3>Portfolio</h3>
+                    </NavLink>
+                </div>
+            </div>
+        </div>
     )
 }
 
