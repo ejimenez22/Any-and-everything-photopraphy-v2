3cbdf0ad89e450dd71c8f83d5d66aaad6ca4dcde
Enrique Jimenez <ejimenez2292@gmail.com>
2022-10-23 18:22:39 -0500

    working on portfolio...sam will you style it :(

--- any/src/components/Portfolio/index.js
+++ any/src/components/Portfolio/index.js
@@ -1,31 +1,139 @@
 import React from "react";
 import Nav from "../Nav";
-import Photolist from "../Photos";
 
 import airshow1 from '../../assets/images/Airshow/0.jpg'
 import airshow2 from '../../assets/images/Airshow/1.jpg'
 import airshow3 from '../../assets/images/Airshow/2.jpg'
+import xmas1 from '../../assets/images/Christmas/0.jpg'
+import xmas2 from '../../assets/images/Christmas/1.jpg'
+import concert1 from '../../assets/images/Concert/0.jpg'
+import landscape1 from '../../assets/images/Landscape/0.jpg'
+import landscape2 from '../../assets/images/Landscape/1.jpg'
+import landscape3 from '../../assets/images/Landscape/2.jpg'
+import landscape4 from '../../assets/images/Landscape/3.jpg'
+import landscape5 from '../../assets/images/Landscape/4.jpg'
+import landscape6 from '../../assets/images/Landscape/5.jpg'
+import portrait1 from '../../assets/images/Portraits/0.jpg'
+import portrait2 from '../../assets/images/Portraits/1.jpg'
+import portrait3 from '../../assets/images/Portraits/2.jpg'
+import portrait4 from '../../assets/images/Portraits/3.jpg'
+import portrait5 from '../../assets/images/Portraits/4.jpg'
+import portrait6 from '../../assets/images/Portraits/5.jpg'
+import portrait7 from '../../assets/images/Portraits/6.jpg'
+import portrait8 from '../../assets/images/Portraits/7.jpg'
+import portrait9 from '../../assets/images/Portraits/8.jpg'
+import portrait10 from '../../assets/images/Portraits/9.jpg'
+import portrait11 from '../../assets/images/Portraits/10.jpg'
+import portrait12 from '../../assets/images/Portraits/11.jpg'
+import portrait13 from '../../assets/images/Portraits/12.jpg'
+import portrait14 from '../../assets/images/Portraits/13.jpg'
+import portrait15 from '../../assets/images/Portraits/14.jpg'
+import portrait16 from '../../assets/images/Portraits/15.jpg'
+import portrait17 from '../../assets/images/Portraits/16.jpg'
+import portrait18 from '../../assets/images/Portraits/17.jpg'
+import portrait19 from '../../assets/images/Portraits/18.jpg'
+import portrait20 from '../../assets/images/Portraits/19.jpg'
+import portrait21 from '../../assets/images/Portraits/20.jpg'
+import portrait22 from '../../assets/images/Portraits/21.jpg'
+import portrait23 from '../../assets/images/Portraits/22.jpg'
+import portrait24 from '../../assets/images/Portraits/23.jpg'
+import portrait25 from '../../assets/images/Portraits/24.jpg'
+import portrait26 from '../../assets/images/Portraits/25.jpg'
+import portrait27 from '../../assets/images/Portraits/26.jpg'
+import racing1 from '../../assets/images/Racing/0.jpg'
+import racing2 from '../../assets/images/Racing/1.jpg'
+import racing3 from '../../assets/images/Racing/2.jpg'
+import racing4 from '../../assets/images/Racing/3.jpg'
+import racing5 from '../../assets/images/Racing/4.jpg'
+import racing6 from '../../assets/images/Racing/5.jpg'
+import racing7 from '../../assets/images/Racing/6.jpg'
+import racing8 from '../../assets/images/Racing/7.jpg'
+import racing9 from '../../assets/images/Racing/8.jpg'
+import racing10 from '../../assets/images/Racing/9.jpg'
+import racing11 from '../../assets/images/Racing/10.jpg'
+import racing12 from '../../assets/images/Racing/11.jpg'
+import racing13 from '../../assets/images/Racing/12.jpg'
+import racing14 from '../../assets/images/Racing/13.jpg'
+import racing15 from '../../assets/images/Racing/14.jpg'
+import recreation1 from '../../assets/images/Recreation/0.jpg'
+import softball1 from '../../assets/images/Softball/0.jpg'
+import softball2 from '../../assets/images/Softball/1.jpg'
+import softball3 from '../../assets/images/Softball/2.jpg'
 
-function Portfolio(props) {
-    const {currentCategory} = props
+
+function Portfolio() {
 
     return (
         <>
         <Nav />
-        <div>
+        <div className="container">
+        <div className='header'>
             <h1>My Work</h1>
         </div>
-
+        <div className="box">
         <div className="portfolio-photos">
-            <img src='./'
-        </div>
-
-
-        {/*
-        <section>
-            <Photolist category={currentCategory.name} />
-        </section>
-        */}
+            <img src={airshow1} alt='airshow' />
+            <img src={airshow2} alt='airshow' />
+            <img src={airshow3} alt='airshow' />
+            <img src={xmas1} alt='christmas' />
+            <img src={xmas2} alt='christmas' />
+            <img src={concert1} alt='concert' />
+            <img src={landscape1} alt='landscape' />
+            <img src={landscape2} alt='landscape' />
+            <img src={landscape3} alt='landscape' />
+            <img src={landscape4} alt='landscape' />
+            <img src={landscape5} alt='landscape' />
+            <img src={landscape6} alt='landscape' />
+            <img src={portrait1} alt='portrait' />
+            <img src={portrait2} alt='portrait' />
+            <img src={portrait3} alt='portrait' />
+            <img src={portrait4} alt='portrait' />
+            <img src={portrait5} alt='portrait' />
+            <img src={portrait6} alt='portrait' />
+            <img src={portrait7} alt='portrait' />
+            <img src={portrait8} alt='portrait' />
+            <img src={portrait9} alt='portrait' />
+            <img src={portrait10} alt='portrait' />
+            <img src={portrait11} alt='portrait' />
+            <img src={portrait12} alt='portrait' />
+            <img src={portrait13} alt='portrait' />
+            <img src={portrait14} alt='portrait' />
+            <img src={portrait15} alt='portrait' />
+            <img src={portrait16} alt='portrait' />
+            <img src={portrait17} alt='portrait' />
+            <img src={portrait18} alt='portrait' />
+            <img src={portrait19} alt='portrait' />
+            <img src={portrait20} alt='portrait' />
+            <img src={portrait21} alt='portrait' />
+            <img src={portrait22} alt='portrait' />
+            <img src={portrait23} alt='portrait' />
+            <img src={portrait24} alt='portrait' />
+            <img src={portrait25} alt='portrait' />
+            <img src={portrait26} alt='portrait' />
+            <img src={portrait27} alt='portrait' />
+            <img src={racing1} alt='racing' />
+            <img src={racing2} alt='racing' />
+            <img src={racing3} alt='racing' />
+            <img src={racing4} alt='racing' />
+            <img src={racing5} alt='racing' />
+            <img src={racing6} alt='racing' />
+            <img src={racing7} alt='racing' />
+            <img src={racing8} alt='racing' />
+            <img src={racing9} alt='racing' />
+            <img src={racing10} alt='racing' />
+            <img src={racing11} alt='racing' />
+            <img src={racing12} alt='racing' />
+            <img src={racing13} alt='racing' />
+            <img src={racing14} alt='racing' />
+            <img src={racing15} alt='racing' />
+            <img src={recreation1} alt='recreation' />
+            <img src={softball1} alt='softball' />
+            <img src={softball2} alt='softball' />
+            <img src={softball3} alt='softball' />
+            </div>
+            </div>
+            </div>
+            
         </>
     )
 }
